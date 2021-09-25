39e963124196092f60699385ae978202b153c562
kevinpark30 <cykevin30@gmail.com>
2021-09-25 13:06:18 -0400

    Discord bot final

--- .vscode/bot.py
+++ .vscode/bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import youtube_dl
 
-# numbers for the poll
+# number characters for the poll
 numbers = ("1️⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣")
 
 # load in the bot token from the .env file
@@ -37,7 +37,11 @@ async def on_member_join(member):
 
 # BOT_COMMMAND: to roll a number between 1 and the given number
 @bot.command(name="roll", help="Rolls a number between 1 and the given number")
-async def roll(ctx, max_roll: int):
+async def roll(ctx, max_roll):
+    try:
+        max_roll = int(max_roll)
+    except:
+        await ctx.send("You have to supply an integer number.")
     if max_roll <= 0:
         await ctx.send("You have to supply a number greater than or equal to 1.")
     roll = random.choice(range(1, max_roll + 1))
@@ -73,45 +77,36 @@ async def poll(ctx, question, *options):
         for reaction in numbers[:len(options)]:
             await poll.add_reaction(reaction)
 
-# BOT_COMMAND: plays the song in the given url of a YouTube video
+# BOT_COMMAND: plays the song with the given url of a YouTube video
 @bot.command(name="play", help="Plays the song with the given url from YouTube")
 async def play(ctx, url: str):
-    # check if there is a song file
-    song_exists = os.path.isfile("song.mp3")
-    try:
-        # if the song file is there, remove it (we need to play a new song since this event is called)
-        if song_exists:
-            os.remove("song.mp3")
-    except:
-        # this means that we were trying to remove the song while it was playing
-        await ctx.send("Wait till the song ends or use the '$stop' command to end the song.")
-
-
     # get the VoiceState of the user who wrote the message 
     user = ctx.author
     voice_state = user.voice
 
-    # check if the user who typed the command is in a voice channel, the bot will go to that voice channel only if the user is there
+    # check if the user who typed the command is in a voice channel, the bot will join the user's voice channel and play music only if the user is in a voice channel
     if voice_state == None:
        await ctx.send(f"{user.name} is not in a voice channel.")
     else:
-        voice_channel = voice_state.channel
+        # check if there is a song file
+        song_exists = os.path.isfile("song.webm")
+        try:
+            # if the song file is there, remove it (we need to play a new song since this event is called)
+            if song_exists:
+                os.remove("song.webm")
+        except:
+            # this means that we were trying to remove the song while it was playing
+            await ctx.send("Wait till the song ends or use the '$stop' command to end the song.")
 
         # check if the bot is already connected to a voice channel and join the channel if not
         voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
         if voice_client == None:
+            voice_channel = voice_state.channel
             await voice_channel.connect()
-        else:
-            await ctx.send("Dooly is already in a voice channel.")
 
         # create youtube_dl options
         youtube_dl_options = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec' : 'mp3',
-            'preferredquality': '192',
-            }],
+            'format': '249',
         }
 
         with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
@@ -119,12 +114,12 @@ async def play(ctx, url: str):
             ydl.download([url])
         
         for file in os.listdir("./"):
-            # find the file that ends with .mp3 and rename it to song.mp3 so that we can find it later
-            if file.endswith(".mp3"):
-                os.rename(file, "song.mp3")
+            # find the file that ends with .webm and rename it to song.webm so that we can find it later
+            if file.endswith(".webm"):
+                os.rename(file, "song.webm")
                 
         voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        voice_client.play(discord.FFmpegPCMAudio("song.mp3"))
+        voice_client.play(discord.FFmpegOpusAudio("song.webm"))
 
 # BOT_COMMAND: makes the bot leave from a voice channel
 @bot.command(name="leave", help="Disconnects the bot from a voice channel")
@@ -167,4 +162,49 @@ async def stop(ctx):
         voice_client.stop()
     else:
         await ctx.send("Dooly is not in a voice channel.")
+
+# BOT_COMMAND: Dooly tells people to play KEAFFA
+@bot.command(name="keaffa", help="Tells everyone to play KEAFFA")
+async def keaffa(ctx):
+    await ctx.send("are we playing keaffa tonight???")
+
+# BOT_COMMAND: Dooly tells people to play KEAFFA (2)
+@bot.command(name="keaffa2", help="Tells everyone to play KEAFFA")
+async def keaffa2(ctx):
+    await ctx.send("are we KEAFING???")
+
+# BOT_COMMAND: Dooly tells people to play KEAFFA (3)
+@bot.command(name="keaffa3", help="Tells everyone to play KEAFFA")
+async def keaffa3(ctx):
+    await ctx.send("ARE\n"+ "WE\n" + "KEAFING\n" + "TODAY?\n" + "PLEASE\n" + "I DONT WANT TO PLAY TFT")
+
+# BOT_COMMAND: Dooly tells people to play amogus
+@bot.command(name="amogus", help="Tells everyone to play amogus")
+async def amogus(ctx):
+    await ctx.send("AMONG US @8PM\n" + "CANCEL ALLLLLLLL YOUR PLANS\n" + "DONT EVEN THINK ABOUT DOING\n" + "HOMEWORK")
+
+# BOT_COMMAND: Dooly sends doomfingers picture
+@bot.command(name="doomfingers", help="sends doomfingers")
+async def doomfingers(ctx):
+    filepath = os.getcwd() + "\\Pictures\\doomfingers.png"
+    await ctx.send(file=discord.File(filepath))
+
+# BOT_COMMAND: Dooly sends maplestory gif
+@bot.command(name="maplestory", help="sends maplestory gif")
+async def maplestory(ctx):
+    filepath = os.getcwd() + "\\Pictures\\maple-story-jon.gif"
+    await ctx.send(file=discord.File(filepath))
+
+# BOT_COMMAND: Dooly sends attention picture
+@bot.command(name="overwatch", help="Tells everyone to play Overwatch")
+async def overwatch(ctx):
+    filepath = os.getcwd() + "\\Pictures\\overwatch.png"
+    await ctx.send(file=discord.File(filepath))
+
+# BOT_COMMAND: Dooly sends cringe gif
+@bot.command(name="cringe", help="Tells someone they are cringe")
+async def cringe(ctx):
+    filepath = os.getcwd() + "\\Pictures\\cringe.gif"
+    await ctx.send(file=discord.File(filepath))
+
 bot.run(TOKEN)
